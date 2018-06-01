73a240c3102e52c2d8cb92cee0902be00f8d6ddb
Ryan Wholey <rjwholey@gmail.com>
2018-06-01 01:00:08 -0700

    another

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@ if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]
 then
   echo "doing ssh magic"
 
-  mkdir -p ./ssh
-  touch ./tmp/ssh.pub
-  chmod 644 ./tmp/ssh.pub
-  echo $SSH >> ./tmp/ssh.pub
-  ssh  -o StrictHostKeyChecking=no -i ./tmp/ssh.pub root@$OPEN_ESL_QA "touch doot"
+  mkdir -p ssh
+  touch tmp/ssh.pub
+  chmod 644 tmp/ssh.pub
+  echo $SSH >> tmp/ssh.pub
+  ssh  -o StrictHostKeyChecking=no -i tmp/ssh.pub root@$OPEN_ESL_QA "touch doot"
 
 else
   echo "pr pr pr"
