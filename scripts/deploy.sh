479dc9d41986bad7148531ba97f3557f7a064896
Ryan Wholey <rjwholey@gmail.com>
2018-06-01 00:56:28 -0700

    another

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,15 @@
 echo "checking whether to deploy..."
 
-echo $TRAVIS_PULL_REQUEST
-echo $TRAVIS_BRANCH
-
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]
 then
-  echo "deploying"
-  touch ./tmp/ssh-pub
-  chmod 644 ./tmp/ssh-pub
+  echo "doing ssh magic"
+
+  mkdir -p ./ssh
+  touch ./tmp/ssh.pub
+  chmod 644 ./tmp/ssh.pub
   echo $SSH >> ./tmp/ssh.pub
-  ssh  -o StrictHostKeyChecking=no -i ./tmp/ssh.pub root@$OPEN_ESL_QA "echo \"hello world\""
+  ssh  -o StrictHostKeyChecking=no -i ./tmp/ssh.pub root@$OPEN_ESL_QA "touch doot"
+
 else
   echo "pr pr pr"
 fi
