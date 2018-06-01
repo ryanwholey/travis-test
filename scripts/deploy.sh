ad352a95dd414f69452cc4b7d7bab310eaee5a02
Ryan Wholey <rjwholey@gmail.com>
2018-06-01 00:26:33 -0700

    another

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ echo "deploying"
 echo $TRAVIS_PULL_REQUEST
 echo $TRAVIS_BRANCH
 
-if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]
 then
   echo "merged to master"
 else
